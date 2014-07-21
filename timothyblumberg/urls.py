fa2b794c2fbcf8408b5f2f444386033c7938d57a
Timothy Blumberg <tblumberg39@gmail.com>
2014-07-21 12:38:06 -0700

    Beginnings of CSS

--- timothyblumberg/urls.py
+++ timothyblumberg/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'timothyblumberg.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', 'timothyblumberg.views.home'),
     url(r'^about$', 'timothyblumberg.views.about')
 )
--- timothyblumberg/views.py
+++ timothyblumberg/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def home(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('main.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
 
