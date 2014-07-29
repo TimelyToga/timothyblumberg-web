487f9d1a5e610f1450dd28dd7439316c09198d5c
Timothy Blumberg <tblumberg39@gmail.com>
2014-07-29 16:44:48 -0700

    Added LearnThings. Increased descriptions

--- timothyblumberg/urls.py
+++ timothyblumberg/urls.py
@@ -17,4 +17,6 @@ urlpatterns = patterns('',
     (r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': timothyblumberg.settings.CSS_DIR}),
     (r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root': timothyblumberg.settings.IMG_DIR}),
     (r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': timothyblumberg.settings.JS_DIR}),
+    (r'^favicon.ico$', 'django.views.static.serve',
+     {'path': 'favicon.ico', 'document_root': timothyblumberg.settings.STATIC_DIR, 'show_indexes': False} ),
 )
