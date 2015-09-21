c0507f3b54fa47952fed72e912db3812d198f64d
TimelyToga <tblumberg39@gmail.com>
2015-09-21 01:54:17 -0400

    Added click hand and instructions to click

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ window.onload=function() {
     $("#personal_logo").fadeTo("slow",1);
 
     $('#personal_logo').click(function(e){
-    $('#personal_logo').fadeOut('slow', function(){
+    $('#logo_wrapper').fadeOut('slow', function(){
         $('#bod_wrapper').fadeTo('slow', 1);
     });
 });
