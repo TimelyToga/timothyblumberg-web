8b4a6e039251bb0ebf37934229272afb3d41fe27
TimelyToga <tblumberg39@gmail.com>
2016-12-05 00:29:52 -0500

    Resume page is donezo BABY

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,8 +1,24 @@
 /**
  * Created by Tim on 04/12/16.
  */
-$(document).ready(function() {
+$(window).load(function() {
+    var line = $('#timeline_line');
     $('.timeline_content').click(function(){
-        $('p', this).slideToggle();
+        var p = jQuery(this).children('.timeline_entry_description');
+        var thisthis = this;
+
+        p.slideToggle(function(){
+            // Increase line height
+            var newHeight = 0;
+            // alert(p.css("display"))
+            if(p.css("display") == "none"){
+                newHeight = line.outerHeight() - $('.timeline_entry_description', thisthis).outerHeight();
+            } else {
+                newHeight = line.outerHeight() + $('.timeline_entry_description', thisthis).outerHeight();
+            }
+
+            line.animate({height: newHeight}, function(){
+            });
+        })
     });
 });
